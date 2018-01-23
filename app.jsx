04ed29e3ba6db1c510576458761303726179b0ba
José L. Junior Garza <correojunior@gmail.com>
2018-01-23 20:12:30 +0000

    JSX

--- app.jsx
+++ app.jsx
@@ -1,12 +1,40 @@
-var element = <h1>Hello World </h1>;
+var str = "World!"
 
-var elementWithOutJSX = React.createElement(
-        'h1',
-        null,
-        'Hello World!'
-    );
+var item = {
+  name: "Cheese",
+  price: 5
+}
+
+var element = <h1> Hello {str}</h1>
+
+//var element = <h1> Hello {item.name} : ${item.price} </h1>
+
+//var element = <div>The Area is: {calculateArea(2,4)}</div>
+
+function calculateArea(x, y) {
+  return x * y
+}
+
+
+var element = <button className ="deleteButton"> Delete </button>;
+
+var product = {name:"apple",stock:0}
+
+var element = <img src ={product.name}></img>
+
+//Do not do this
+//var element = <img src ="{product.imageURL}"></img>
+
+
+var element = (
+  <div>
+        <div>Hello World</div>
+        <div>Hello World</div>
+    </div>
+
+)
 
 ReactDOM.render(
-  elementWithOutJSX,
+  element,
   document.getElementById("root")
 );
